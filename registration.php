d349588e0d33e9bb8c213b3c3f98aad0b2c498d3
hugorince <hugo.rince@gmail.com>
2023-02-14 11:42:59 +0100

    registration setup

---
+++ registration.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>ReSoC - Inscription</title>
+    <meta name="author" content="Julien Falconnet">
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <?php
+    include('header.html');
+    include('connect.php');
+    ?>
+    <div id="wrapper">
+
+        <aside>
+            <h2>Présentation</h2>
+            <p>Bienvenue sur notre réseau social.</p>
+        </aside>
+        <main>
+            <article>
+                <h2>Inscription</h2>
+                <?php
+
+                $enCoursDeTraitement = isset($_POST['email']);
+                if ($enCoursDeTraitement) {
+                    $new_email = $_POST['email'];
+                    $new_alias = $_POST['pseudo'];
+                    $new_passwd = $_POST['motpasse'];
+
+                    //Etape 4 : Petite sécurité
+                    // pour éviter les injection sql : https://www.w3schools.com/sql/sql_injection.asp
+                    $new_email = $mysqli->real_escape_string($new_email);
+                    $new_alias = $mysqli->real_escape_string($new_alias);
+                    $new_passwd = $mysqli->real_escape_string($new_passwd);
+                    // on crypte le mot de passe pour éviter d'exposer notre utilisatrice en cas d'intrusion dans nos systèmes
+                    $new_passwd = md5($new_passwd);
+                    // NB: md5 est pédagogique mais n'est pas recommandée pour une vraies sécurité
+                    //Etape 5 : construction de la requete
+                    $lInstructionSql = "INSERT INTO users (id, email, password, alias) "
+                        . "VALUES (NULL, "
+                        . "'" . $new_email . "', "
+                        . "'" . $new_passwd . "', "
+                        . "'" . $new_alias . "'"
+                        . ");";
+                    // Etape 6: exécution de la requete
+                    $ok = $mysqli->query($lInstructionSql);
+                    if (!$ok) {
+                        echo "L'inscription a échoué : " . $mysqli->error;
+                    } else {
+                        echo "Votre inscription est un succès : " . $new_alias;
+                        echo " <a href='login.php'>Connectez-vous.</a>";
+                    }
+                }
+                ?>
+                <form action="registration.php" method="post">
+
+                    <dl>
+                        <dt><label for='pseudo'>Pseudo</label></dt>
+                        <dd><input type='text' name='pseudo'></dd>
+                        <dt><label for='email'>E-Mail</label></dt>
+                        <dd><input type='email' name='email'></dd>
+                        <dt><label for='motpasse'>Mot de passe</label></dt>
+                        <dd><input type='password' name='motpasse'></dd>
+                    </dl>
+                    <input type='submit'>
+                </form>
+            </article>
+        </main>
+    </div>
+</body>
+
+</html>
