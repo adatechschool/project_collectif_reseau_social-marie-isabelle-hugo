7f8a6be34f163f4899f1aef0eb400d1ccf561c05
hugorince <hugo.rince@gmail.com>
2023-02-14 11:58:15 +0100

    login temporaire

---
+++ login.php
@@ -0,0 +1,77 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>ReSoC - Connexion</title>
+    <meta name="author" content="Julien Falconnet">
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <?php
+    include('header.html');
+    include('connect.php');
+    ?>
+
+    <div id="wrapper">
+
+        <aside>
+            <h2>Présentation</h2>
+            <p>Bienvenue sur notre réseau social.</p>
+        </aside>
+        <main>
+            <article>
+                <h2>Connexion</h2>
+                <?php
+
+                $enCoursDeTraitement = isset($_POST['email']);
+                if ($enCoursDeTraitement) {
+                    $emailAVerifier = $_POST['email'];
+                    $passwdAVerifier = $_POST['motpasse'];
+                    $emailAVerifier = $mysqli->real_escape_string($emailAVerifier);
+                    $passwdAVerifier = $mysqli->real_escape_string($passwdAVerifier);
+                    $passwdAVerifier = md5($passwdAVerifier);
+
+                    $lInstructionSql = "SELECT * "
+                        . "FROM users "
+                        . "WHERE "
+                        . "email LIKE '" . $emailAVerifier . "'"
+                    ;
+
+                    $res = $mysqli->query($lInstructionSql);
+                    $user = $res->fetch_assoc();
+                    if (!$user or $user["password"] != $passwdAVerifier) {
+                        echo "La connexion a échouée. ";
+
+                    } else {
+                        echo "Votre connexion est un succès : " . $user['alias'] . ".";
+                        // Etape 7 : Se souvenir que l'utilisateur s'est connecté pour la suite
+                        // documentation: https://www.php.net/manual/fr/session.examples.basic.php
+                        $_SESSION['connected_id'] = $user['id'];
+                    }
+                }
+                ?>
+                <form action="login.php" method="post">
+                    <dl>
+                        <dt><label for='email'>E-Mail</label></dt>
+                        <dd><input type='email' name='email'></dd>
+                        <dt><label for='motpasse'>Mot de passe</label></dt>
+                        <dd><input type='password' name='motpasse'></dd>
+                    </dl>
+                    <input type='submit'>
+                </form>
+                <p>
+                    Pas de compte?
+                    <a href='registration.php'>Inscrivez-vous.</a>
+                </p>
+
+            </article>
+        </main>
+    </div>
+</body>
+
+</html>
