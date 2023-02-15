4d1547f2a0c93c75c3d32d5da5dc691c0128f616
hugorince <hugo.rince@gmail.com>
2023-02-15 14:28:05 +0100

    login synchronisé

--- login.php
+++ login.php
@@ -1,3 +1,32 @@
+<?php
+include('connect.php');
+$isConnected = false;
+$enCoursDeTraitement = isset($_POST['email']);
+if ($enCoursDeTraitement) {
+    $emailAVerifier = $_POST['email'];
+    $passwdAVerifier = $_POST['motpasse'];
+
+    $emailAVerifier = $mysqli->real_escape_string($emailAVerifier);
+    $passwdAVerifier = $mysqli->real_escape_string($passwdAVerifier);
+    // on crypte le mot de passe pour éviter d'exposer notre utilisatrice en cas d'intrusion dans nos systèmes
+    $passwdAVerifier = md5($passwdAVerifier);
+    // NB: md5 est pédagogique mais n'est pas recommandée pour une vraie sécurité
+    //Etape 5 : construction de la requete
+    $lInstructionSql = "SELECT * FROM users WHERE email LIKE '$emailAVerifier' ";
+    // Etape 6: Vérification de l'utilisateur
+    $res = $mysqli->query($lInstructionSql);
+    $user = $res->fetch_assoc();
+    if (!$user or $user["password"] != $passwdAVerifier) {
+        echo "La connexion a échoué. ";
+
+    } else {
+        echo "Votre connexion est un succès : " . $user['alias'] . ".";
+        $_SESSION['connected_id'] = $user['id'];
+        $isConnected = true;
+    }
+}
+
+?>
 <!doctype html>
 <html lang="fr">
 
@@ -9,13 +38,13 @@
 </head>
 
 <body>
+
     <?php
     include('header.php');
-    include('connect.php');
+
     ?>
 
     <div id="wrapper">
-
         <aside>
             <h2>Présentation</h2>
             <p>Bienvenue sur notre réseau social.</p>
@@ -23,44 +52,13 @@
         <main>
             <article>
                 <h2>Connexion</h2>
-                <?php
-                /**
-                 * TRAITEMENT DU FORMULAIRE
-                 */
-                // Etape 1 : vérifier si on est en train d'afficher ou de traiter le formulaire
-                // si on recoit un champs email rempli il y a une chance que ce soit un traitement
-                $enCoursDeTraitement = isset($_POST['email']);
-                if ($enCoursDeTraitement) {
-                    $emailAVerifier = $_POST['email'];
-                    $passwdAVerifier = $_POST['motpasse'];
+                <p>
+                    <?php if ($isConnected) {
+                        echo "Tues connecté : " . $user['alias'];
+                    } ?>
+                </p>
 
-
-                    //Etape 4 : Petite sécurité
-                    // pour éviter les injection sql : https://www.w3schools.com/sql/sql_injection.asp
-                    $emailAVerifier = $mysqli->real_escape_string($emailAVerifier);
-                    $passwdAVerifier = $mysqli->real_escape_string($passwdAVerifier);
-                    // on crypte le mot de passe pour éviter d'exposer notre utilisatrice en cas d'intrusion dans nos systèmes
-                    $passwdAVerifier = md5($passwdAVerifier);
-                    // NB: md5 est pédagogique mais n'est pas recommandée pour une vraie sécurité
-                    //Etape 5 : construction de la requete
-                    $lInstructionSql = "SELECT * FROM users WHERE email LIKE '$emailAVerifier' ";
-
-                    // Etape 6: Vérification de l'utilisateur
-                    $res = $mysqli->query($lInstructionSql);
-                    $user = $res->fetch_assoc();
-                    if (!$user or $user["password"] != $passwdAVerifier) {
-                        echo "La connexion a échoué. ";
-
-                    } else {
-                        echo "Votre connexion est un succès : " . $user['alias'] . ".";
-                        // Etape 7 : Se souvenir que l'utilisateur s'est connecté pour la suite
-                        // documentation: https://www.php.net/manual/fr/session.examples.basic.php
-                        $_SESSION['connected_id'] = $user['id'];
-                    }
-                }
-                ?>
                 <form action="login.php" method="post">
-                    <input type="hidden" name="???" value="achanger">
                     <dl>
                         <dt><label for='email'>E-Mail</label></dt>
                         <dd><input type='email' name='email'></dd>
@@ -77,6 +75,7 @@
             </article>
         </main>
     </div>
+
 </body>
 
 </html>
