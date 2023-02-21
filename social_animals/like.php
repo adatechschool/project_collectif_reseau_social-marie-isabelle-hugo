89f6d57c3cbcd5362329b0a47f6f5285ccd69691
MarieKosDuc <marie.koscianski@gmail.com>
2023-02-21 12:35:33 +0100

    Creating Profil.php content

---
+++ social_animals/like.php
@@ -0,0 +1,31 @@
+<?php 
+$postId = $post['ID'];
+
+// get post's likes
+$getPostLikes = "COUNT post_id FROM likes WHERE post_id = $postID";
+
+
+// verify if post has been liked by user
+
+$hasBeenLikedSQL = "SELECT post_id FROM likes WHERE user_id = $connectedUserId AND post_id = $postId";
+$hasBeenLikedInfo = $mysqli->query($hasBeenLikedSQL);
+$hasBeenLiked = $hasBeenLikedInfo->fetch_assoc();
+
+if ($hasBeenLiked) {
+    $liked = true;
+} else {
+    $liked = false;
+}
+
+$beenClicked = isset($_POST[$postId]);
+if ($beenClicked) {
+    $hasNotBeenLikedSQL = "
+            INSERT INTO likes (post_id, user_id)
+        VALUES ($postId, $connectedUser)
+            ";
+    $hasNotBeenLikedInfo = $mysqli->query($hasNotBeenLikedSQL);
+    $liked = true;
+}
+
+?>
+
