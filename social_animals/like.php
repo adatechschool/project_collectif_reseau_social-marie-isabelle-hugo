9cc220ef7ac1167626989a31eddaa1d9aed105a4
MarieKosDuc <marie.koscianski@gmail.com>
2023-02-21 16:57:10 +0100

    Fixed like button

--- social_animals/like.php
+++ social_animals/like.php
@@ -1,46 +1,26 @@
 <?php 
-$postId = $post['ID'];
+if (isset($_POST['getPostId'])){
 
+    // variable set
+    $postId = $_POST['getPostId'];
 
-// get post's likes
-$getPostLikesRequest = "SELECT COUNT(post_id) as likenumber FROM likes WHERE post_id = $postId" ;
-$getPostLikes = $mysqli->query($getPostLikesRequest);
-$postLikes = $getPostLikes->fetch_assoc();
+    if(isset($_POST["like"])){
+            $insertLikeRequest = "INSERT INTO likes (user_id, post_id) VALUES ($connectedUserId, $postId) ";
+            $ok = $mysqli->query($insertLikeRequest);
+            if (!$ok) {
+                echo "Error :" . $mysqli->error;
+                }
+        };
 
+    if (isset($_POST["unlike"])){
+            $removeLikeRequest = "DELETE FROM likes WHERE user_id = $connectedUserId AND post_id = $postId";
+            var_dump($removeLikeRequest);
+            $ok = $mysqli->query($removeLikeRequest);
+            if (!$ok) {
+                echo "Error :" . $mysqli->error;
+            }
+        };
 
-// verify if post has been liked by user
-$hasBeenLikedSQL = "SELECT post_id FROM likes WHERE user_id = $connectedUserId AND post_id = $postId";
-$hasBeenLikedInfo = $mysqli->query($hasBeenLikedSQL);
-$hasBeenLiked = $hasBeenLikedInfo->fetch_assoc();
+    ?>
 
-if ($hasBeenLiked) {
-    $liked = true;
-} else {
-    $liked = false;
-}
-
-$beenClicked = isset($_POST[$postId]);
-if ($beenClicked) {
-    $hasNotBeenLikedSQL = "INSERT INTO likes (user_id, post_id) VALUES ($connectedUser, $postId) ";
-    $hasNotBeenLikedInfo = $mysqli->query($hasNotBeenLikedSQL);
-    $liked = true;
-}
-
-// get actual page URL for refresh
-$actualPageUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-
-
-?>
-
-
-<!-- HTML -->
-
-<div>
-    <?php var_dump($postLikes); 
-        echo $postLikes['likenumber'];
-        echo $actualPageUrl;?>
-</div>
-
-<form method="post" action='<?php echo $actualPageUrl ?>'>
-    <input type="submit" name=<?php echo $postId ?> value="♥ <?php echo $post['like_number']; ?>">
-</form>
+<?php } ?>
---
+++ social_animals/likebutton.php
@@ -0,0 +1,35 @@
+<?php 
+
+// Getting the post ID
+$postId = $post['ID'];
+
+// get post's likes
+$getPostLikesRequest = "SELECT COUNT(post_id) as like_number FROM likes WHERE post_id = $postId" ;
+$getPostLikes = $mysqli->query($getPostLikesRequest);
+$postLikes = $getPostLikes->fetch_assoc();
+
+
+// verify if post has been liked by user
+$hasBeenLikedSQL = "SELECT post_id FROM likes WHERE user_id = $connectedUserId AND post_id = $postId";
+$hasBeenLikedInfo = $mysqli->query($hasBeenLikedSQL);
+$hasBeenLiked = $hasBeenLikedInfo->fetch_assoc();
+
+// get actual page URL for refresh
+$actualPageUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+
+if ($hasBeenLiked){ ?>
+<form method="post" action='<?php echo $actualPageUrl ?>'>
+    <input type="hidden" name="getPostId" value='<?php echo $postId ?>'>
+    <input type="submit" name="unlike" value="♥ <?php echo $postLikes['like_number']; ?>"
+    class="cursor-pointer">
+</form>
+
+<?php } else { ?>
+    <form method="post" action='<?php echo $actualPageUrl ?>'>
+    <input type="hidden" name="getPostId" value='<?php echo $postId ?>'>
+    <input type="submit" name="like" value="♥ <?php echo $postLikes['like_number']; ?>"
+    class="font-extrabold cursor-pointer">
+</form>
+
+<?php } ?>
