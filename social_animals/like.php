dc49e398610df6dff78f106f67be7c4947797e09
MarieKosDuc <marie.koscianski@gmail.com>
2023-02-21 15:07:31 +0100

    First like integration in feed

--- social_animals/like.php
+++ social_animals/like.php
@@ -1,14 +1,14 @@
 <?php 
 $postId = $post['ID'];
 
+
 // get post's likes
-$getPostLikesRequest = "COUNT post_id FROM likes WHERE post_id = $postID";
+$getPostLikesRequest = "SELECT COUNT(post_id) as likenumber FROM likes WHERE post_id = $postId" ;
 $getPostLikes = $mysqli->query($getPostLikesRequest);
 $postLikes = $getPostLikes->fetch_assoc();
 
 
 // verify if post has been liked by user
-
 $hasBeenLikedSQL = "SELECT post_id FROM likes WHERE user_id = $connectedUserId AND post_id = $postId";
 $hasBeenLikedInfo = $mysqli->query($hasBeenLikedSQL);
 $hasBeenLiked = $hasBeenLikedInfo->fetch_assoc();
@@ -21,13 +21,26 @@ if ($hasBeenLiked) {
 
 $beenClicked = isset($_POST[$postId]);
 if ($beenClicked) {
-    $hasNotBeenLikedSQL = "
-            INSERT INTO likes (post_id, user_id)
-        VALUES ($postId, $connectedUser)
-            ";
+    $hasNotBeenLikedSQL = "INSERT INTO likes (user_id, post_id) VALUES ($connectedUser, $postId) ";
     $hasNotBeenLikedInfo = $mysqli->query($hasNotBeenLikedSQL);
     $liked = true;
 }
 
+// get actual page URL for refresh
+$actualPageUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+
 ?>
 
+
+<!-- HTML -->
+
+<div>
+    <?php var_dump($postLikes); 
+        echo $postLikes['likenumber'];
+        echo $actualPageUrl;?>
+</div>
+
+<form method="post" action='<?php echo $actualPageUrl ?>'>
+    <input type="submit" name=<?php echo $postId ?> value="♥ <?php echo $post['like_number']; ?>">
+</form>
