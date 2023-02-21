dc5117f86b6af3f4bb42df5e21bc3378409089fa
hugorince <hugo.rince@gmail.com>
2023-02-21 15:03:46 +0100

    post editor in feed

--- social_animals/header.php
+++ social_animals/header.php
@@ -12,15 +12,15 @@
             <div class="relative group">
                 <div
                     class="flex items-center cursor-pointer text-4xl group-hover:border-grey-light rounded-t-lg py-1 px-2 ">
-                    <a href="profile.php">
-                        My Profile</a>
+
+                    My Profile
                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                         <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
                     </svg>
                 </div>
                 <div
                     class="bg-yellow-100 items-center absolute border border-t-0 rounded-b-lg p-1 bg-white p-2 invisible group-hover:visible w-full">
-                    <a href="#" class="px-4 py-2 block text-black hover:bg-grey-lighter">View Profile</a>
+                    <a href="profile.php" class="px-4 py-2 block text-black hover:bg-grey-lighter">View Profile</a>
                     <hr class="border-t mx-2 border-grey-ligght">
                     <a href="index.php" class="px-4 py-2 block text-black hover:bg-grey-lighter">Logout</a>
                 </div>
--- social_animals/profile.php
+++ social_animals/profile.php
@@ -1,48 +1,59 @@
-<?php include ('connect.php');
+<?php include('connect.php');
 
-    // Differenciating the connected user from the wall's owner
+$myWall = true;
+$otherUsersWall = false;
+
+// Differenciating the connected user from the wall's owner
+if (isset($_GET['user_id'])) {
     $wallOwnerId = intval($_GET['user_id']);
-    $connectedUserId = $_SESSION['connected_id'];
-
     $myWall = false;
-    $otherUsersWall = false;
+}
 
 
-    // If the user is on their wall, get all their info
-    if ($connectedUserId == $wallOwnerId) {
-        $userInfosRequest = "SELECT * FROM users WHERE users.id = '$connectedUserId'  ";
-        $getUserInfos = $mysqli->query($userInfosRequest);
-        if (!$getUserInfos) {
-            echo ("Échec de la requete : " . $mysqli->error);
-            }
-        $user = $getUserInfos->fetch_assoc(); 
-
-        // If the user is browsing another user's wall, get this user's info
-        } else {
-        $userInfosRequest = "SELECT * FROM users WHERE users.id = '$wallOwnerId'  ";
-        $getUserInfos = $mysqli->query($userInfosRequest);
-        if (!$getUserInfos) {
-            echo ("Échec de la requete : " . $mysqli->error);
-            }
-        $user = $getUserInfos->fetch_assoc(); 
-        }
-
-    // Get the user's type (cat, dog...)
-    $type = intval($user['type_id']);
-    $userTypeRequest = "SELECT * FROM type WHERE type.ID = $type ";
-    $getUserType = $mysqli->query($userTypeRequest);
-    if (!$getUserType) {
+// If the user is on their wall, get all their info
+if ($connectedUserId) {
+    $userInfosRequest = "SELECT * FROM users WHERE users.id = '$connectedUserId'  ";
+    $getUserInfos = $mysqli->query($userInfosRequest);
+    if (!$getUserInfos) {
         echo ("Échec de la requete : " . $mysqli->error);
-        }
-    $userType = $getUserType->fetch_assoc(); 
+    }
+    $user = $getUserInfos->fetch_assoc();
 
-        // Get the user's posts
+    // If the user is browsing another user's wall, get this user's info
+} else {
+    $userInfosRequest = "SELECT * FROM users WHERE users.id = '$wallOwnerId'  ";
+    $getUserInfos = $mysqli->query($userInfosRequest);
+    if (!$getUserInfos) {
+        echo ("Échec de la requete : " . $mysqli->error);
+    }
+    $user = $getUserInfos->fetch_assoc();
+}
+
+// Get the user's type (cat, dog...)
+$type = intval($user['type_id']);
+$userTypeRequest = "SELECT * FROM type WHERE type.ID = $type ";
+$getUserType = $mysqli->query($userTypeRequest);
+if (!$getUserType) {
+    echo ("Échec de la requete : " . $mysqli->error);
+}
+$userType = $getUserType->fetch_assoc();
+
+// Get the user's posts
+if (isset($_GET['user_id'])) {
     $userId = intval($user['ID']);
-    $userPostsRequest = "SELECT * FROM posts WHERE posts.user_id = '$userId' ";
+    $userPostsRequest = "SELECT * FROM posts WHERE posts.user_id = $userId ";
     $getUserPosts = $mysqli->query($userPostsRequest);
     if (!$getUserPosts) {
         echo ("Échec de la requete : " . $mysqli->error);
-        }
+    }
+} else {
+    $userId = $connectedUserId;
+    $userPostsRequest = "SELECT * FROM posts WHERE posts.user_id = $userId ";
+    $getUserPosts = $mysqli->query($userPostsRequest);
+    if (!$getUserPosts) {
+        echo ("Échec de la requete : " . $mysqli->error);
+    }
+}
 ?>
 
 
@@ -72,49 +83,68 @@
                 <main class="flex flex-col">
 
 
-                <!-- User's informations -->
+                    <!-- User's informations -->
                     <div id="userDetails" class="flex justify-center items-center space-x-4">
                         <!-- <div id="pictureContainer" class="w-30 h-30 rounded-full"> -->
-                            <img class="object-cover object-center w-40 h-40 rounded-full" src="upload/<?php echo $user['photo'] ?>" alt="Profile picture">
-                            <div> <?php echo $user['name']; ?> </div>
-                            <div><?php echo $userType["label"];?></div>
-                            <div><?php echo $user["email"];?></div>
+                        <img class="object-cover object-center w-40 h-40 rounded-full"
+                            src="upload/<?php echo $user['photo'] ?>" alt="Profile picture">
+                        <div>
+                            <?php echo $user['name']; ?>
+                        </div>
+                        <div>
+                            <?php echo $userType["label"]; ?>
+                        </div>
+                        <div>
+                            <?php echo $user["email"]; ?>
+                        </div>
+                    </div>
+                    <div>
+                        <?php include('posteditor.php') ?>
+                        <div class="">
+                            <form action="profile.php" enctype="multipart/form-data" method="post"
+                                class="flex flex-col space-y-2 space-x-8 justify-center items-center border-black border-2 bg-lime-50 mt-4">
+                                <label for="user_picture" class="mt-2">Choose a picture</label>
+                                <input type="file" name="user_picture" />
+                                <p>Add a description</p>
+                                <textarea name="description" id="" cols="30" rows="2"></textarea>
+                                <input type="submit" value="Post"
+                                    class="bg-orange-300 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+                            </form>
+                        </div>
                     </div>
 
-
-
-                <!-- New post form if user is on their wall -->
-                <?php if ($myWall == true){
+                    <!-- New post form if user is on their wall -->
+                    <?php if ($myWall == true) {
 
                     // ------------- insertion du formulaire new post -----------------
-
+                
                 } ?>
-                
 
-                <!-- User's older posts -->
-                <!---------------- CSS A FAIRE  ------------>
-                <article id='users-posts'>
-                
-                <?php while ($post = $getUserPosts->fetch_assoc()) {
-                ?>
-                <article>
-                    <h3>
-                        <time datetime='2020-02-01'>
-                            <?php echo $post['date']; ?>
-                        </time>
-                    </h3>
-                    <div>
-                    <div id='image-container' class="w-64 h-64">
-                        <img src="upload/<?php echo $post['photo']; ?>" alt="post image" class="object-cover w-64 h-64 rounded-xl">
-                    </div>  
-                    <div> <?php echo $post['description']; ?>
-                    </div> 
-                    
-                    <?php include('like.php'); ?>
+                    <!-- User's older posts -->
+                    <!---------------- CSS A FAIRE  ------------>
+
+                    <?php while ($post = $getUserPosts->fetch_assoc()) {
+                        ?>
+                        <article>
+                            <h3>
+                                <time datetime='2020-02-01'>
+                                    <?php echo $post['date']; ?>
+                                </time>
+                            </h3>
+                            <div>
+                                <div id='image-container' class="w-64 h-64">
+                                    <img src="upload/<?php echo $post['photo']; ?>" alt="post image"
+                                        class="object-cover w-64 h-64 rounded-xl">
+                                </div>
+                                <div>
+                                    <?php echo $post['description']; ?>
+                                </div>
+
+                                <!-- <?php include('like.php'); ?> -->
 
 
-                </article>
-                <?php } ?>
+                        </article>
+                    <?php } ?>
 
 
                 </main>
