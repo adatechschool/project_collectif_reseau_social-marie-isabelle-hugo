5652b1aa6d2168db1b376e26f7fb972578f34801
MarieKosDuc <marie.koscianski@gmail.com>
2023-02-22 10:30:01 +0100

    Added styling to like button

--- social_animals/profile.php
+++ social_animals/profile.php
@@ -92,7 +92,7 @@ if (isset($_POST['unfollow'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Social animals</title>
+    <title>Social Animals</title>
     <link href="output.css" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
 
